d942ec31701ddb4697af833f3777a34f84413f01
mrcraig <1002386c@student.gla.ac.uk>
2013-11-06 18:01:19 +0000

    no nullpointer YAAAAAAY

--- Task_4/src/KeyValue.java
+++ Task_4/src/KeyValue.java
@@ -1,9 +1,19 @@
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
 import java.util.Comparator;
 
+import org.apache.hadoop.io.Writable;
 
-public class KeyValue {
+
+public class KeyValue implements Writable {
 	private long key;
 	private int value;
+	
+	public KeyValue(){
+		this.key=0;
+		this.value=0;
+	}
 	
 	public KeyValue(long key, int value){
 		this.key = key;
@@ -25,5 +35,19 @@ public class KeyValue {
 			return 1;
 		else 
 			return 0;
+	}
+
+	@Override
+	public void readFields(DataInput in) throws IOException {
+		this.key = in.readLong();
+		this.value = in.readInt();
+		
+	}
+
+	@Override
+	public void write(DataOutput out) throws IOException {
+		out.writeLong(key);
+		out.writeInt(value);
+		
 	}
 }
--- Task_4/src/Reduce2.java
+++ Task_4/src/Reduce2.java
@@ -14,16 +14,16 @@ public class Reduce2 extends Reducer<IntWritable, KeyValue, LongWritable, IntWri
 	public void reduce(IntWritable key, Iterable<KeyValue> value, Context context) throws IOException, InterruptedException {
 		//Add collection storage
 		ArrayList<KeyValue> sortArray = new ArrayList<KeyValue>(150);
-		//Comparator comp = new KVCompare();
+		Comparator comp = new KVCompare();
 		
 		
 		while(value.iterator().hasNext()){
 			sortArray.add(value.iterator().next());
 		}
 		
-		//Collections.sort(sortArray, comp);
+		Collections.sort(sortArray, comp);
 		int noElems = context.getConfiguration().getInt("recordstodisplay", 0);
-		for(int i=0;i<2;i++){
+		for(int i=0;i<noElems;i++){
 			context.write(new LongWritable(sortArray.get(i).getKey()), new IntWritable(sortArray.get(i).getValue()));
 		}
 		
